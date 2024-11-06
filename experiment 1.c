fe65df5399ccf5578408a4ff1b6ea47382636162
Ismail2908 <iambatman@eng.rizvi.edu.in>
2024-11-06 15:04:25 +0530

    Create experiment 1.c

---
+++ experiment 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("Full Name\t: Ismail Shaikh\n");
+    printf("Age\t\t: 26\n");
+    printf("Class\t\t: FE\n");
+    printf("Division\t: B\n");
+    printf("UIN\t\t: 241C013\n");
+
+    return 0;
+}
